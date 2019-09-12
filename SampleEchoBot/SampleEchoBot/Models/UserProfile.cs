339c601ce524aae52e627f4e94b3bbe0d43f7f64
Aarav <aaravuno8@gmail.com>
2019-09-12 21:33:58 +0530

    Add files html and bot

---
+++ SampleEchoBot/SampleEchoBot/Models/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleEchoBot.Models
+{
+    public class UserProfile
+    {
+        public string Name { get; set; }
+
+    }
+}
---
+++ SampleEchoBot/SampleEchoBot/States/WelcomeUserState.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace SampleEchoBot.States
+{
+    // Stores User Welcome state for the conversation.
+    // Stored in "Microsoft.Bot.Builder.ConversationState" and
+    // backed by "Microsoft.Bot.Builder.MemoryStorage".
+    public class WelcomeUserState
+    {
+        // Gets or sets whether the user has been welcomed in the conversation.
+        public bool DidBotWelcomeUser { get; set; } = false;
+    }
+}
