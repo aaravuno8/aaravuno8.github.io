f504eca22071220114c0c1c7f812f8aa9a630071
aaravuno8 <46205338+aaravuno8@users.noreply.github.com>
2021-05-02 12:14:29 +0530

    Update index.js

--- Face/index.js
+++ Face/index.js
@@ -1,10 +1,10 @@
 const video = document.getElementById("video")
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+    faceapi.nets.tinyFaceDetector.loadFromUri('Face/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('Face/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('Face/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('Face/models')
 ]).then(startVideo())
 
 function startVideo(){
